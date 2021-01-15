03de94121c9eabc7d37546f58ae5b6d8ad3681c7
Lukas Siemon <simlu@users.noreply.github.com>
2021-01-15 11:41:17 -0800

    amend: simplification
    Untested, but this should work exactly the same

--- src/vue-app/scanners/backlinks.js
+++ src/vue-app/scanners/backlinks.js
@@ -1,19 +1,8 @@
 import objectScan from "object-scan";
 
-const getAllBacklinks = async (input) => {
-	let results = [];
-
-	objectScan(["**.type"], {
-		filterFn: ({ value, parent }) => {
-			const isLinkNode = value === "zettelkasten_link";
-
-			if (isLinkNode) {
-				results.push(parent);
-			}
-		}
-	})(input);
-
-	return results;
-};
+const getAllBacklinks = async (input) => objectScan(["**.type"], {
+	rtn: 'parent',
+	filterFn: ({ value }) => value === "zettelkasten_link"
+})(input);
 
 export default getAllBacklinks;
